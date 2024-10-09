056acc1a020e2097a8c8975ca13c81e4e1971e08
Manuel Pereira <66897822+manilex2@users.noreply.github.com>
2024-10-09 10:49:44 -0500

    Cambio en main.ts para manejo de CORS.

--- functions/vanu/src/main.ts
+++ functions/vanu/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 import { AppModule } from './app.module';
-import * as admin from 'firebase-admin';
 import { https, setGlobalOptions } from 'firebase-functions/v2';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { Express } from 'express-serve-static-core';
+import { INestApplication } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
@@ -15,8 +17,9 @@ setGlobalOptions({
 });
 
 const expressServer = express();
+let nestApp: INestApplication;
 
-const createFunction = async (expressInstance: Express): Promise<void> => {
+const createFunction = async (expressInstance: Express) => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
@@ -35,3 +38,34 @@ export const api = https.onRequest(
     expressServer(request, response);
   },
 );
+
+const createFunction = async (expressInstance: Express) => {
+  if (!nestApp) {
+    // Evita inicialización repetida
+    nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressInstance),
+    );
+
+    const corsOptions: CorsOptions = {
+      origin: [
+        'https://vanu-coh-az-knifgq.flutterflow.app',
+        'https://app.flutterflow.io/debug',
+        'https://aion-crm-asm.web.app',
+        'https://app.vanushop.com',
+      ], // Lista de orígenes permitidos
+      methods: 'GET, POST, PUT, DELETE, OPTIONS', // Métodos HTTP permitidos
+      allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
+      credentials: true, // Si deseas habilitar cookies o autenticación
+    };
+    nestApp.enableCors(corsOptions); // Configura CORS en NestJS
+    await nestApp.init(); // Inicializa la aplicación NestJS
+  }
+  return nestApp;
+};
+
+// Exporta la función Firebase
+export const api = https.onRequest(async (request, response) => {
+  await createFunction(expressServer); // Inicializa NestJS solo si no está ya inicializado
+  expressServer(request, response); // Maneja la solicitud con el servidor Express
+});
